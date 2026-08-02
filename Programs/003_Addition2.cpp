62a5a80a22b304150a495c4b754f841610bfaa13
Vaishnavi Garad <vaishnavibg2504@gmail.com>
2026-08-02 10:22:41 +0530

    Add basic addition program in C++

---
+++ Programs/003_Addition2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i = 0,j = 0,ans;
+
+    cout<<"Enter First Number : "<<endl;
+    cin>>i;
+
+    cout<<"Enter Second Number : "<<endl;
+    cin>>j;
+
+    ans = i + j ;
+
+    cout<<"Addition is : "<<ans<<endl;
+    
+    return 0;
+}
